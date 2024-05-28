83009ac0fbe0905b3bf9c3036df22e056c5f444c
kailashsur <kailashsurofficial@gmail.com>
2024-05-28 19:34:33 +0530

    google search console

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ function Document() {
   return (
     <Html lang="en">
       <Head />
+      <meta name="google-site-verification" content="3AUqLkaoS5pRSGC1dDM378S1nEQh5nimfeVvF_rEOYc" />
       <body>
         <div id="skip-navigation" />
         <Main />
